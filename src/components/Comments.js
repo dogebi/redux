50be5de0d35d8bec2558111afe75568759f8c0a4
dogebi <rainxlh@gmail.com>
2021-07-31 10:44:19 +0800

    delete effect {}

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { fetchComments } from "../redux/index";
 
 const Comments = ({ fetchComments, loading, comments }) => {
-  useEffect(({ fetchComments }) => {
+  useEffect(() => {
     fetchComments();
   }, []);
 
